f240477ed80ee71f02637dd8a9fa8be5d5d6e3ac
LeonidGarasko <djjiehuh@gmail.com>
2022-05-29 17:17:31 +0300

    123

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,11 @@
+// Напиши скрипт, который для каждого элемента массива ingredients:
+
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul.ingredients.
+
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +14,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+ingredients.forEach(el => {
+  const ingredient = document.createElement('li');
+  ingredient.classList.add('item');
+  ingredient.textContent = el;
+
+  const ingredientList = document.querySelector('#ingredients');
+ingredientList.appendChild(ingredient);
+})
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector('.login-form');
+const emeilEl = document.querySelector('input[type="emeil"]');
+const passwordEl = document.querySelector('input[type="password]');
 
+form.addEventListener('submit', onFormSubmit);
+
+let user = { emeil: '', password: '' };
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    console.log(formData);
+
+    if (emeilEl === '' || passwordEl === '') {
+       return alert('All fields must be filled!');
+    } 
+    else {
+        user.emeil = emeilEl;
+        user.password = passwordEl;
+    }
+    document.querySelector('.login-form').reset();
+    console.log(user)
+}
